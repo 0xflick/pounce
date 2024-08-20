cddfef621f8d3c39ba5a7c03f2937f289da83eef
alex flick <alexsflick@gmail.com>
2024-08-20 01:36:27 -0400

    Squashed commit of the following:
    commit 4672281345d45ae1dfffe63d96a7185687f0b5b6
    Author: alex flick <alexsflick@gmail.com>
    Date:   Tue Aug 20 01:35:25 2024 -0400
    
        append dirty to version

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,17 @@
 const VERSION: &str = env!("VERGEN_GIT_DESCRIBE");
+const DIRTY: &str = env!("VERGEN_GIT_DIRTY");
 const BUILD_DATE: &str = env!("VERGEN_BUILD_DATE");
 const OPT_LEVEL: &str = env!("VERGEN_CARGO_OPT_LEVEL");
 
 pub fn engine_name() -> String {
     let release_type = if OPT_LEVEL == "3" { "release" } else { "dev" };
     let date = BUILD_DATE.replace("-", "");
-    format!("pounce {}-{}-{}", release_type, date, VERSION)
+
+    let version = if DIRTY == "true" {
+        format!("{}-dirty", VERSION)
+    } else {
+        VERSION.to_string()
+    };
+
+    format!("pounce {}-{}-{}", release_type, date, version)
 }
