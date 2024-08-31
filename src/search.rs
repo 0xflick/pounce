7021b5cb4fdcc59fa2410ee5c8ca29e55acb2487
alex flick <alexsflick@gmail.com>
2024-08-31 13:08:17 -0400

    iir

--- src/search.rs
+++ src/search.rs
@@ -241,6 +241,11 @@ impl Search {
 
         let static_eval = tt_eval.unwrap_or(self.position.eval());
 
+        // internal iterative reduction
+        if !is_root && is_pv && depth >= 4 && !self.position.in_check() && tt_move == Move::NONE {
+            depth -= 1;
+        }
+
         // Null move pruning
         if !is_pv
             && depth >= 3
