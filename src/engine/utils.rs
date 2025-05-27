faa8f1fe751cb3b9d508dfccd66b3282b8563c73
alex flick <alexsflick@gmail.com>
2025-05-27 14:05:35 -0400

    use cargo version if git version is unavailable

--- src/engine/utils.rs
+++ src/engine/utils.rs
@@ -1,4 +1,5 @@
-const VERSION: &str = env!("VERGEN_GIT_DESCRIBE");
+const GIT_VERSION: &str = env!("VERGEN_GIT_DESCRIBE");
+const CARGO_VERSION: &str = env!("CARGO_PKG_VERSION");
 const DIRTY: &str = env!("VERGEN_GIT_DIRTY");
 const BUILD_DATE: &str = env!("VERGEN_BUILD_DATE");
 const OPT_LEVEL: &str = env!("VERGEN_CARGO_OPT_LEVEL");
@@ -7,10 +8,13 @@ pub fn engine_name() -> String {
     let release_type = if OPT_LEVEL == "3" { "release" } else { "dev" };
     let date = BUILD_DATE.replace("-", "");
 
-    let version = if DIRTY == "true" {
-        format!("{}-dirty", VERSION)
+    #[allow(clippy::const_is_empty)]
+    let version = if GIT_VERSION.is_empty() || GIT_VERSION == "VERGEN_IDEMPOTENT_OUTPUT" {
+        return format!("pounce {}-{}-{}", release_type, date, CARGO_VERSION);
+    } else if DIRTY == "true" {
+        format!("{}-dirty", GIT_VERSION)
     } else {
-        VERSION.to_string()
+        GIT_VERSION.to_string()
     };
 
     format!("pounce {}-{}-{}", release_type, date, version)
