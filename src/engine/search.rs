859ef79d242522aefe34aed1a70c33ef88b6f262
alex flick <alexsflick@gmail.com>
2025-08-07 16:33:17 -0400

    restore delta prune

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -520,11 +520,18 @@ impl<'a> Search<'a> {
         let mut best = stand_pat;
         let mut best_move = Move::NONE;
 
-        // see margin
-        let margin = (alpha as i32 - stand_pat as i32 - 300).max(1);
-
-        let mut move_picker = MovePicker::new_quiescence(&self.position, tt_move, margin);
+        let mut move_picker = MovePicker::new_quiescence(&self.position, tt_move, 15);
         while let Some(mv) = move_picker.next(&self.position, &self.history) {
+            // delta pruning
+            if let Some(captured) = mv.captured_role(&self.position) {
+                if mv.promotion().is_none()
+                    && !self.position.in_check()
+                    && ((stand_pat + 500 + eval::PIECE_VALUES_EG[captured] as i16) < alpha)
+                    && self.position.non_pawn_material(self.position.side)
+                {
+                    continue;
+                }
+            }
             self.position.make_move(mv);
             let score = -self.quiescence_search(-beta, -alpha, is_pv);
             self.position.unmake_move(mv);
