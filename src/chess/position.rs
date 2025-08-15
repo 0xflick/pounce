e7fc383bfdb2b32a9ddeab58a536aa7b9d3f4ad1
alex flick <alexsflick@gmail.com>
2025-08-15 17:15:16 -0400

    inline and lints

--- src/chess/position.rs
+++ src/chess/position.rs
@@ -465,12 +465,12 @@ impl Position {
         }
 
         // update their castling rights
-        if let Some(captured) = state.captured {
-            if captured.role == Role::Rook {
-                self.key.toggle_castling(self.castling);
-                self.castling.discard_square(to);
-                self.key.toggle_castling(self.castling);
-            }
+        if let Some(captured) = state.captured
+            && captured.role == Role::Rook
+        {
+            self.key.toggle_castling(self.castling);
+            self.castling.discard_square(to);
+            self.key.toggle_castling(self.castling);
         }
 
         self.update_checks_and_pins(mv, Some(mv.promotion().unwrap_or(piece.role)));
@@ -485,14 +485,13 @@ impl Position {
 
         // handle ep square here (after updating checks and pins)
         // only set ep square if the attacker if the ep move is legal
-        if let Some(ep_sq) = potential_ep_sq {
-            if (ep_attackers & !self.pinned[self.side]).any()
-                && (!self.checkers[self.side].any()
-                    || self.checkers[self.side] == Bitboard::from(ep_sq.down(self.side).unwrap()))
-            {
-                self.ep_square = Some(ep_sq);
-                self.key.toggle_ep(self.ep_square);
-            }
+        if let Some(ep_sq) = potential_ep_sq
+            && (ep_attackers & !self.pinned[self.side]).any()
+            && (!self.checkers[self.side].any()
+                || self.checkers[self.side] == Bitboard::from(ep_sq.down(self.side).unwrap()))
+        {
+            self.ep_square = Some(ep_sq);
+            self.key.toggle_ep(self.ep_square);
         }
     }
 
