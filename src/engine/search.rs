3479e1f433f14fb5e84e0b5113f4e23cc2da0728
alex flick <alexsflick@gmail.com>
2025-08-21 13:20:53 -0400

    cleanup: remove delta pruning (#78)
    ```
    no-delta [completed]
    no-delta (h@16mb th@1) vs main (h@16mb th@1)
    
    elo = 10.63 [-1.21, 22.54] (95%)
    llr = 2.94 (accepted) | sprt = (-10.00, 0.00) [-2.94, 2.94]
    wdl = 469/923/408 (26.1%/51.3%/22.7%) | penta = 38/216/353/233/60
    games = 1800
    ```

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 
 use arrayvec::ArrayVec;
 
-use crate::chess::{Accumulator, Color, GameResult, Move, Position, Role, Square};
+use crate::chess::{Accumulator, Color, GameResult, Move, Position, Square};
 use crate::engine::eval::{self, nnue};
 use crate::engine::limits::Limits;
 use crate::engine::movepicker::{MAX_MOVES, MovePicker};
@@ -595,39 +595,6 @@ impl<'a> Search<'a> {
 
         let mut best = stand_pat;
         let mut best_move = Move::NONE;
-
-        if !self.position.in_check() {
-            let best_case_score = {
-                let mut value = eval::PIECE_VALUES[Role::Pawn as usize];
-
-                for role in ((Role::Pawn as usize)..=(Role::Queen as usize)).rev() {
-                    if self
-                        .position
-                        .by_color_role(self.position.side.opponent(), Role::new(role as u8))
-                        .any()
-                    {
-                        value = eval::PIECE_VALUES[role];
-                        break;
-                    }
-                }
-
-                // check for promotions
-                if (self.position.by_color_role(self.position.side, Role::Pawn)
-                    & self.position.side.opponent().home_rank())
-                .any()
-                {
-                    value += eval::PIECE_VALUES[Role::Queen as usize]
-                        - eval::PIECE_VALUES[Role::Pawn as usize];
-                }
-
-                value
-            };
-
-            let delta_margin = alpha.saturating_sub(stand_pat).saturating_sub(425) as i32;
-            if best_case_score < delta_margin {
-                return stand_pat;
-            }
-        }
 
         let tt_move = tt_hit.map_or(Move::NONE, |tt| tt.best_move);
 
