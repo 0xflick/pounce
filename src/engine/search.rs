1fd69ecfe7e1db664c7c15924aee4b4320baa3ec
alex flick <alexsflick@gmail.com>
2025-08-20 20:22:11 -0400

    who knows now

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -545,13 +545,24 @@ impl<'a> Search<'a> {
         if self.position.in_check() {
             stand_pat = -eval::INFINITY;
         } else if let Some(entry) = tt_hit {
-            if entry.static_eval != eval::NO_VALUE {
-                stand_pat = denormalize_score(entry.static_eval, MAX_PLY);
+            if entry.score != eval::NO_VALUE {
+                stand_pat = denormalize_score(entry.score, MAX_PLY);
+            } else if entry.static_eval != eval::NO_VALUE {
+                stand_pat = entry.static_eval;
             } else {
                 stand_pat = eval::score_nnue(&self.position, &self.accum);
             }
         } else {
             stand_pat = eval::score_nnue(&self.position, &self.accum);
+
+            self.tt.store(Entry::new(
+                self.position.key,
+                0,
+                stand_pat,
+                stand_pat,
+                EntryType::Exact,
+                Move::NONE,
+            ));
         }
 
         if stand_pat >= beta {
@@ -591,7 +602,7 @@ impl<'a> Search<'a> {
                 value
             };
 
-            let delta_margin = alpha.saturating_sub(stand_pat).saturating_sub(425) as i32;
+            let delta_margin = alpha.saturating_sub(stand_pat).saturating_sub(200) as i32;
             if best_case_score < delta_margin {
                 return stand_pat;
             }
@@ -599,7 +610,7 @@ impl<'a> Search<'a> {
 
         let tt_move = tt_hit.map_or(Move::NONE, |tt| tt.best_move);
 
-        let see_margin = alpha.saturating_sub(stand_pat).saturating_sub(500).max(1) as i32;
+        let see_margin = alpha.saturating_sub(stand_pat).saturating_sub(200).max(1) as i32;
         let mut move_picker = MovePicker::new_quiescence(&self.position, tt_move, see_margin);
         while let Some(mv) = move_picker.next(&self.position, &self.history) {
             self.position.make_move_with(mv, &mut self.accum);
