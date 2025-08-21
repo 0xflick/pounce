e01121bfe73c6125a79c1003d1fba0e74d45cfd7
alex flick <alexsflick@gmail.com>
2025-08-21 14:38:37 -0400

    try 250

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -599,7 +599,7 @@ impl<'a> Search<'a> {
         let tt_move = tt_hit.map_or(Move::NONE, |tt| tt.best_move);
 
         // Only consider captures that are enough to raise alpha + margin
-        let see_margin = alpha.saturating_sub(stand_pat).saturating_sub(150).max(1) as i32;
+        let see_margin = alpha.saturating_sub(stand_pat).saturating_sub(250).max(1) as i32;
         let mut move_picker = MovePicker::new_quiescence(&self.position, tt_move, see_margin);
         while let Some(mv) = move_picker.next(&self.position, &self.history) {
             self.position.make_move_with(mv, &mut self.accum);
