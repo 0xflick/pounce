0b47eea60b3ffbf297e436aa02e684e80bb498b9
alex flick <alexsflick@gmail.com>
2025-08-22 09:05:56 -0400

    much smaller margin

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -638,7 +638,7 @@ impl<'a> Search<'a> {
         let tt_move = tt_hit.map_or(Move::NONE, |tt| tt.best_move);
 
         // Only consider tacticals that are enough to raise alpha + margin
-        let see_margin = alpha.saturating_sub(stand_pat).saturating_sub(250).max(1) as i32;
+        let see_margin = alpha.saturating_sub(stand_pat).saturating_sub(50).max(1) as i32;
         let mut move_picker = MovePicker::new_quiescence(&self.position, tt_move, see_margin);
         while let Some(mv) = move_picker.next(self, MAX_PLY) {
             self.position.make_move_with(mv, &mut self.accum);
