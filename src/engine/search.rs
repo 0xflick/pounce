d37609ad47eaa0c8e6f57bac433eefb3560e7724
alex flick <alexsflick@gmail.com>
2025-08-26 15:28:59 -0600

    Update search.rs

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -21,7 +21,7 @@ pub fn init_reductions() {
         #[allow(clippy::needless_range_loop)]
         for m in 1..MAX_MOVES {
             for depth in 1..MAX_DEPTH as usize {
-                let reduction = 0.7 + ((depth as f32).ln() * (m as f32).ln()) / 2.6;
+                let reduction = 0.6 + ((depth as f32).ln() * (m as f32).ln()) / 2.1;
                 REDUCTIONS[depth][m] = reduction as u8;
             }
         }
